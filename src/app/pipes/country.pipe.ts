b039af0692beaf23754a9501c7203ab60a5ad6f1
tugbayavuzz <tugbayavuz@yandex.com>
2021-01-06 13:58:49 +0300

    searc and filter, country list added

--- src/app/pipes/country.pipe.ts
+++ src/app/pipes/country.pipe.ts
@@ -7,7 +7,7 @@ export class CountryPipe implements PipeTransform {
 
   transform(value: any, ...args: any[]): any {
     if(args[0]){
-      return value.filter((n) => n.country.trim().toLowerCase().includes(args[0]))
+      return value.filter((n) => n.country.trim().toLowerCase().includes(args[0].trim()))
     }
     else{
       return value;
